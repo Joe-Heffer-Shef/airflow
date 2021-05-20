83f69d451b466138385b2f13e8cbc77b79195e66
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-05-20 13:55:56 +0100

    build datacake workflow

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 
 ## Testing
 
+Test a specific task
+
+```bash
+# docker-compose exec airflow-worker airflow task test <dag> <task> <date>
+docker-compose exec --user airflow airflow-worker airflow tasks test airbods raw_04a7d0f3-855e-481e-8bf2-8f4c4bb7049c 2021-05-20
+```
+
+
+
+Run unit tests
+
 ```bash
 docker-compose exec airflow-worker python -m unittest --failfast
 ```
--- dags/airbods.py
+++ dags/airbods.py
@@ -3,6 +3,7 @@ import textwrap
 import json
 
 import airflow
+from airflow.operators.python import PythonOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.http.hooks.http import HttpHook
 
@@ -14,10 +15,24 @@ class GraphQLHttpOperator(SimpleHttpOperator):
         self.query = query
 
     def execute(self, context: dict):
-        self.data = dict(query=self.render_template(content=self.query,
-                                                    context=context))
-        text = super().execute(context)
-        return json.loads(text)['data']
+        # Build template context
+        _context = self.params.copy()
+        _context.update(context)
+        rendered_query = self.render_template(content=self.query,
+                                              context=_context)
+        self.log.debug(rendered_query)
+        self.data = dict(query=rendered_query)
+        return super().execute(context)
+
+
+def save_file(task_instance, params, **kwargs):
+    previous_task_id = 'raw_{}'.format(params['id'])
+    data = task_instance.xcom_pull(task_ids=previous_task_id)
+
+    filename = '{}.json'.format(params['id'])
+    with open(filename, 'w') as file:
+        file.write(data)
+        return file.name
 
 
 with airflow.DAG(
@@ -32,6 +47,8 @@ with airflow.DAG(
 query {
   allDevices(inWorkspace:"0bdfb2eb-6531-4afb-a842-ce6b51d3c980") {
     id
+    serialNumber
+    verboseName
   }
 }
 """)))
@@ -39,22 +56,32 @@ query {
     # Iterate over devices
     for device in response.json()['data']['allDevices']:
         # Download raw data for each device
-        task = GraphQLHttpOperator(
-            task_id='raw_{}'.format(device['id']),
-            params=dict(
-                device_id=device['id'],
-            ),
+        task_id = 'raw_{}'.format(device['id'])
+        get_raw_data = GraphQLHttpOperator(
+            http_conn_id='datacake_airbods',
+            task_id=task_id,
+            doc=device['verboseName'],
+            params=device,
             # Jinja escape characters for GraphQL syntax
             query="""
-query {{ '{{' }}
-  device(deviceId: "{{ device_id }}") {{ '{{' }}
+query {{ '{' }}
+  device(deviceId: "{{ id }}") {{ '{' }}
     history(
-      fields: [CO2,TEMPERATURE,LORAWAN_SNR,LORAWAN_DATARATE,LORAWAN_RSSI,AIR_QUALITY,HUMIDITY]
+      fields: ["CO2","TEMPERATURE","LORAWAN_SNR","LORAWAN_DATARATE","LORAWAN_RSSI","AIR_QUALITY","HUMIDITY"]
       timerangestart: "{{ ts }}"
       timerangeend: "{{ next_execution_date }}"
       resolution: "raw"
     ) 
-  {{ '}}' }}
-{{ '}}' }}
+  {{ '}' }}
+{{ '}' }}
 """
         )
+
+        # Saw raw JSON data
+        save_raw_data = PythonOperator(
+            task_id='save_{}'.format(device['id']),
+            python_callable=save_file,
+            params=device,
+        )
+
+        get_raw_data >> save_raw_data
